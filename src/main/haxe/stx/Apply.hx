5c95c0996f6daa3d0ba9cb461cf7e65d948611c7
0b1kn00b <polysemantic@gmail.com>
2014-10-02 17:19:11 +0200

    Abstract Apply for lifting to interface

---
+++ src/main/haxe/stx/Apply.hx
@@ -0,0 +1,13 @@
+package stx;
+
+import stx.apply.AnonymousApply;
+import stx.ifs.Apply in IApply;
+
+@:forward abstract Apply<I,O>(IApply<I,O>) from IApply<I,O> to IApply<I,O>{
+  public function new(v){
+    this = v;
+  }
+  @:from static public function fromAnonymousApply<I,O>(fn:I->O):Apply<I,O>{
+    return new AnonymousApply(fn);
+  }
+}
